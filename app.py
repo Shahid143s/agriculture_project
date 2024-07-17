a3ab6dbfbb3316a4fd3a5ae926e033bb20dbd7ee
Shahid Anowar <exkrogroup@gmail.com>
2024-07-17 13:07:32 +0530

    Prem Photo and Description

--- app.py
+++ app.py
@@ -20,13 +20,14 @@ with st.sidebar:
 
     # Define member information
     members = [
-        {"name": "Shahid Anowar", "img_url": "https://i.ibb.co/6m9Gt2s/IMG20231128135521-modified.png"},
+        {"name": "Shahid Anowar", "img_url": "https://i.ibb.co/qjNqCwF/Picsart-24-07-16-13-37-07-577.png"},
         {"name": "Joyosmita Paul", "img_url": "https://i.ibb.co/nmJkh8K/Whats-App-Image-2024-07-16-at-7-47-30-PM-modified.png"},
         {"name": "Abhisekh Pandit", "img_url": "https://i.ibb.co/vvz18jp/Whats-App-Image-2024-07-16-at-2-24-20-PM-modified.png"},
         {"name": "Prem Kr Sah", "img_url": "https://i.ibb.co/935nGLT/imageedit-1-7430777653-modified.png"},
         {"name": "Manas Seal", "img_url": "https://i.ibb.co/Y30fxQ7/IMG-20240715-173204-modified.png"}
     ]
-#https://i.ibb.co/qjNqCwF/Picsart-24-07-16-13-37-07-577.png
+#https://i.ibb.co/6m9Gt2s/IMG20231128135521-modified.png - Green
+#https://i.ibb.co/qjNqCwF/Picsart-24-07-16-13-37-07-577.png - Wall
     # Add members to columns
     for i, member in enumerate(members):
         col = col1 if i % 2 == 0 else col2
@@ -67,7 +68,7 @@ with col2:
     st.markdown('''<div style='text-align:center;' class="bordered-container">
                     <h3>Plant Disease Classifier <br></h3>
                     <img src='https://i.ytimg.com/vi/zcq5aw9t-Ds/maxresdefault.jpg' alt='Image' style='width:300px;height:200px;' caption="Column 2 Image">
-                    <br>
+                    <br><br><br>
                     <figcaption>The Plant Disease Classifier uses advanced machine learning to accurately identify plant diseases from images, providing crucial early detection and facilitating timely, effective treatment for healthier crops.</figcaption>
                     </div>
                 ''', unsafe_allow_html=True)
