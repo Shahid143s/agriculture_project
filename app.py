e8a6641cbb8dccd0f8ba5efde4d2f801f9b89e49
Shahid Anowar <exkrogroup@gmail.com>
2024-07-17 13:26:26 +0530

    Photos updated

--- app.py
+++ app.py
@@ -20,11 +20,11 @@ with st.sidebar:
 
     # Define member information
     members = [
-        {"name": "Shahid Anowar", "img_url": "https://i.ibb.co/qjNqCwF/Picsart-24-07-16-13-37-07-577.png"},
-        {"name": "Joyosmita Paul", "img_url": "https://i.ibb.co/nmJkh8K/Whats-App-Image-2024-07-16-at-7-47-30-PM-modified.png"},
+        {"name": "Shahid Anowar", "img_url": "https://i.ibb.co/D7G3dWk/IMG-20240717-132015.png"},
+        {"name": "Joyosmita Paul", "img_url": "https://i.ibb.co/SJ82dGh/Whats-App-Image-2024-07-16-at-7-47-30-PM-modified-modified.png"},
         {"name": "Abhisekh Pandit", "img_url": "https://i.ibb.co/vvz18jp/Whats-App-Image-2024-07-16-at-2-24-20-PM-modified.png"},
         {"name": "Prem Kr Sah", "img_url": "https://i.ibb.co/935nGLT/imageedit-1-7430777653-modified.png"},
-        {"name": "Manas Seal", "img_url": "https://i.ibb.co/Y30fxQ7/IMG-20240715-173204-modified.png"}
+        {"name": "Manas Seal", "img_url": "https://i.ibb.co/Sw24jZ7/IMG-20240715-173204-modified-modified.png"}
     ]
 #https://i.ibb.co/6m9Gt2s/IMG20231128135521-modified.png - Green
 #https://i.ibb.co/qjNqCwF/Picsart-24-07-16-13-37-07-577.png - Wall
@@ -53,7 +53,7 @@ with col1:
     st.markdown('''<div style='text-align:center;' class="bordered-container">
                     <h3>Crop Recommendation System</h3>
                     <img src='https://media.licdn.com/dms/image/D5612AQEx2AMyErNfxQ/article-cover_image-shrink_600_2000/0/1710648344420?e=2147483647&v=beta&t=5E0GoftZHK9Bz81pwUehZ0YveQ0vwS7pHxzZKIJepwk' alt='Image' style='width:300px;height:200px;' caption="Column 2 Image">
-                    <br>
+                    <br><br>
                     <figcaption>A Crop Recommendation System leverages soil data and climate conditions to suggest optimal crops for farmers, enhancing yield, sustainability, and profitability with data-driven agricultural practices.</figcaption>
                     </div>
                 ''', unsafe_allow_html=True)
@@ -68,7 +68,7 @@ with col2:
     st.markdown('''<div style='text-align:center;' class="bordered-container">
                     <h3>Plant Disease Classifier <br></h3>
                     <img src='https://i.ytimg.com/vi/zcq5aw9t-Ds/maxresdefault.jpg' alt='Image' style='width:300px;height:200px;' caption="Column 2 Image">
-                    <br><br><br>
+                    <br><br>
                     <figcaption>The Plant Disease Classifier uses advanced machine learning to accurately identify plant diseases from images, providing crucial early detection and facilitating timely, effective treatment for healthier crops.</figcaption>
                     </div>
                 ''', unsafe_allow_html=True)
